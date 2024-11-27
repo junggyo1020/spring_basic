5fde1825fab996bb18410ac6754ef5ddae302eb3
Willy <willy_1020@naver.com>
2024-11-27 21:00:35 +0900

    [section 3-4] 회원 도메인 개발

--- src/main/java/hello/core/member/MemberRepository.java
+++ src/main/java/hello/core/member/MemberRepository.java
@@ -2,7 +2,6 @@ package hello.core.member;
 
 public interface MemberRepository {
 
-    void save(Member member);
-
-    Member findById(Long memberId);
+    void save(Member member); // 회원을 저장
+    Member findById(Long memberId); // 회원을 검색
 }
--- src/main/java/hello/core/member/MemberService.java
+++ src/main/java/hello/core/member/MemberService.java
@@ -2,7 +2,6 @@ package hello.core.member;
 
 public interface MemberService {
 
-    void join(Member mebmer);
-
-    Member findMember(Long memberId);
+    void join(Member member); // 회원 가입
+    Member findMember(Long memberId); // 회원 조회
 }
