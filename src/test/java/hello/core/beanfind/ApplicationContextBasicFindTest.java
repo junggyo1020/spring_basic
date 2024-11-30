ab20395adbd5d0b60a05755336d4948b3c09435a
Willy <willy_1020@naver.com>
2024-11-30 23:04:03 +0900

    [✅section 5-4] 스프링 빈 조회 - 동일한 타입이 둘 이상

--- src/test/java/hello/core/beanfind/ApplicationContextBasicFindTest.java
+++ src/test/java/hello/core/beanfind/ApplicationContextBasicFindTest.java
@@ -10,43 +10,37 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.NoSuchBeanDefinitionException;
-import org.springframework.beans.factory.config.BeanDefinition;
-import org.springframework.boot.autoconfigure.web.reactive.HttpHandlerAutoConfiguration.AnnotationConfig;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-public class ApplicationContextInfoTest {
+class ApplicationContextBasicFindTest {
 
     AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
 
     @Test
-    @DisplayName("빈 이름으로 조회")
+    @DisplayName("빈 이름 조회")
     void findBeanByName() {
         MemberService memberService = ac.getBean("memberService", MemberService.class);
         System.out.println("memberService = " + memberService);
-        System.out.println("memberService.getClass = " + memberService.getClass());
+        System.out.println("memberService.getClass( = " + memberService.getClass());
         assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
     }
 
     @Test
-    @DisplayName("이름없이 타입으로만 조회")
+    @DisplayName("이름 없이 타입으로만 조회")
     void findBeanByType() {
         MemberService memberService = ac.getBean(MemberService.class);
-        System.out.println("memberService = " + memberService);
-        System.out.println("memberService.getClass = " + memberService.getClass());
         assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
     }
 
     @Test
     @DisplayName("구체 타입으로 조회")
     void findBeanByName2() {
-        MemberServiceImpl memberService = ac.getBean("memberService", MemberServiceImpl.class);
-        System.out.println("memberService = " + memberService);
-        System.out.println("memberService.getClass = " + memberService.getClass());
+        MemberService memberService = ac.getBean("memberService", MemberServiceImpl.class);
         assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
     }
 
     @Test
-    @DisplayName("빈 이름으로 조회 X")
+    @DisplayName("빈 이름으로 조회 x")
     void findBeanByNameX() {
         assertThrows(NoSuchBeanDefinitionException.class,
                 () -> ac.getBean("xxxxx", MemberService.class));
